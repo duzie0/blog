abf3037a0d401ea7eec5ede17e56549d8726215d
duzie0 <haosc1994@163.com>
2018-07-04 19:39:18 +0800

    blog 路由

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,7 @@ urlpatterns =[
     url(r'^blog/$',views.blog,name='blog'),
     url(r'^create_blog/$',views.create_blog,name='create_blog'),
     url(r'^update_blog/$',views.update_blog,name='update_blog'),
-    url(r'^blog_page/(?P<pk>[0-9]+)/$',views.blog_page,name='blog_page'),
+    url(r'^blog_page/(?P<pk>[0-9]+)/$',views.blog_page),
+    url(r'^modify_page/(?P<pk>[0-9]+)/$',views.modify_page),
+    url(r'^delete_page/(?P<pk>[0-9]+)/$',views.delete_page),
 ]
