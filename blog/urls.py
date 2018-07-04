873657e8ee8fb530c74a3cce57bed7a8c8e905e8
duzie0 <haosc1994@163.com>
2018-07-04 19:53:42 +0800

    完成修改博客功能了

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns =[
     url(r'^create_blog/$',views.create_blog,name='create_blog'),
     url(r'^update_blog/$',views.update_blog,name='update_blog'),
     url(r'^blog_page/(?P<pk>[0-9]+)/$',views.blog_page),
-    url(r'^modify_page/(?P<pk>[0-9]+)/$',views.modify_page),
-    url(r'^delete_page/(?P<pk>[0-9]+)/$',views.delete_page),
+    url(r'^modify_blog/(?P<pk>[0-9]+)/$',views.modify_blog),
+    url(r'^delete_blog/(?P<pk>[0-9]+)/$',views.delete_blog),
 ]
